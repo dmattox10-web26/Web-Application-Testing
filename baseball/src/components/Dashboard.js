285e92f2b0748128c063316b1216daf9b17add50
Daniel Mattox <dmattox10@gmail.com>
2020-01-16 13:37:58 -0500

    Finished the Dashboard component

--- baseball/src/components/Dashboard.js
+++ baseball/src/components/Dashboard.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import { Jumbotron } from 'reactstrap'
+import { Jumbotron, Row, Col, Button } from 'reactstrap'
 
-const Dashboard = () => {
+const Dashboard = (props) => {
     return (
-       <Jumbotron>
-           
-       </Jumbotron>
+        <Jumbotron>
+            <Row style={{textAlign: 'center'}}>
+                <Col xs='3'>
+                    <Button onClick={ props.strike }>Strike</Button>
+                </Col>
+                <Col xs='3'>
+                    <Button onClick={ props.ball }>Ball</Button>
+                </Col>
+                <Col xs='3'>
+                    <Button onClick={ props.foul }>Foul</Button>
+                </Col>
+                <Col xs='3'>
+                    <Button onClick={ props.hit }>Hit</Button>
+                </Col>
+           </Row>
+        </Jumbotron>
     )
 }
 
